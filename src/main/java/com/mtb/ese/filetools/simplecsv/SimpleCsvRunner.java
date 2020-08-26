13eb47b28500229df1b57dae86b2e21335123efb
Dan Cochran <dan.s.cochran@gmail.com>
2020-08-26 18:16:25 -0400

    cleaned up imports

--- src/main/java/com/mtb/ese/filetools/simplecsv/SimpleCsvRunner.java
+++ src/main/java/com/mtb/ese/filetools/simplecsv/SimpleCsvRunner.java
@@ -3,9 +3,6 @@ package com.mtb.ese.filetools.simplecsv;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.boot.CommandLineRunner;
